9ae08379da5b698d6facc40bd0415de2e254ae9c
amsl <iamyhe@gmail.com>
2012-11-24 11:58:16 +0800

    #1: widgets/textbox: fixed display caret error. #2: old frame rate is inaccurate.
    Signed-off-by: amsl <iamyhe@gmail.com>

--- components/rtgui/include/rtgui/widgets/textbox.h
+++ components/rtgui/include/rtgui/widgets/textbox.h
@@ -1,16 +1,16 @@
 /*
- * File      : textbox.h
- * This file is part of RT-Thread RTOS
- * COPYRIGHT (C) 2006 - 2009, RT-Thread Development Team
- *
- * The license and distribution terms for this file may be
- * found in the file LICENSE in this distribution or at
- * http://www.rt-thread.org/license/LICENSE
- *
- * Change Logs:
- * Date           Author       Notes
- * 2009-10-16     Bernard      first version
- */
+* File      : textbox.h
+* This file is part of RT-Thread RTOS
+* COPYRIGHT (C) 2006 - 2009, RT-Thread Development Team
+*
+* The license and distribution terms for this file may be
+* found in the file LICENSE in this distribution or at
+* http://www.rt-thread.org/license/LICENSE
+*
+* Change Logs:
+* Date           Author       Notes
+* 2009-10-16     Bernard      first version
+*/
 #ifndef __RTGUI_TEXTBOX_H__
 #define __RTGUI_TEXTBOX_H__
 
@@ -22,13 +22,13 @@
 extern "C" {
 #endif
 
-    DECLARE_CLASS_TYPE(textbox);
+DECLARE_CLASS_TYPE(textbox);
 
-    /** Gets the type of a textbox */
+/** Gets the type of a textbox */
 #define RTGUI_TEXTBOX_TYPE       (RTGUI_TYPE(textbox))
-    /** Casts the object to a rtgui_textbox_t */
+/** Casts the object to a rtgui_textbox_t */
 #define RTGUI_TEXTBOX(obj)       (RTGUI_OBJECT_CAST((obj), RTGUI_TEXTBOX_TYPE, rtgui_textbox_t))
-    /** Checks if the object is a rtgui_textbox_t */
+/** Checks if the object is a rtgui_textbox_t */
 #define RTGUI_IS_TEXTBOX(obj)    (RTGUI_OBJECT_CHECK_TYPE((obj), RTGUI_TEXTBOX_TYPE))
 
 #define RTGUI_TEXTBOX_DEFAULT_WIDTH     80
@@ -43,42 +43,45 @@ extern "C" {
 
 #define RTGUI_TEXTBOX_LINE_MAX      128  /* text line cache */
 
-    struct rtgui_textbox
-    {
-        /* inherit from widget */
-        struct rtgui_widget parent;
+struct rtgui_textbox
+{
+	/* inherit from widget */
+	struct rtgui_widget parent;
 
-        /* text box flag */
-        rt_uint32_t flag;
+	/* text box flag */
+	rt_uint32_t flag;
 
-        /* current line and position */
-        rt_uint16_t line, line_begin, position, line_length;
-        rt_uint16_t dis_length; /*may be display length.*/
-        char *text;
-        rt_size_t font_width;
+	/* current line and position */
+	rt_uint16_t line, line_begin, position, line_length;
+	rt_uint16_t dis_length; /*may be display length.*/
+	rt_uint16_t first_pos;
+	char mask_char;
+	char *text;
+	rt_size_t font_width;
 
-        rtgui_timer_t *caret_timer;
-        rtgui_color_t *caret;
-        rtgui_rect_t  caret_rect;
+	rtgui_timer_t *caret_timer;
+	rtgui_color_t *caret;
+	rtgui_rect_t  caret_rect;
 
-        /* textbox private data */
-        rt_bool_t (*on_enter)(struct rtgui_textbox *box, rtgui_event_t *event);
-    };
-    typedef struct rtgui_textbox rtgui_textbox_t;
+	/* textbox private data */
+	rt_bool_t (*on_enter)(struct rtgui_textbox *box, rtgui_event_t *event);
+};
+typedef struct rtgui_textbox rtgui_textbox_t;
 
-    struct rtgui_textbox *rtgui_textbox_create(const char *text, rt_uint32_t flag);
-    void rtgui_textbox_destroy(struct rtgui_textbox *box);
+rtgui_textbox_t *rtgui_textbox_create(const char *text, rt_uint32_t flag);
+void rtgui_textbox_destroy(struct rtgui_textbox *box);
 
-    rt_bool_t rtgui_textbox_event_handler(struct rtgui_object *object, struct rtgui_event *event);
+rt_bool_t rtgui_textbox_event_handler(struct rtgui_object *object, struct rtgui_event *event);
 
-    void rtgui_textbox_set_value(struct rtgui_textbox *box, const char *text);
-    const char *rtgui_textbox_get_value(struct rtgui_textbox *box);
+void rtgui_textbox_set_value(struct rtgui_textbox *box, const char *text);
+const char *rtgui_textbox_get_value(struct rtgui_textbox *box);
+void rtgui_textbox_set_mask_char(rtgui_textbox_t *box, const char ch);
+const char rtgui_textbox_get_mask_char(rtgui_textbox_t *box);
+void rtgui_textbox_set_line_length(struct rtgui_textbox *box, rt_size_t length);
 
-    void rtgui_textbox_set_line_length(struct rtgui_textbox *box, rt_size_t length);
+void rtgui_textbox_get_edit_rect(struct rtgui_textbox *box, rtgui_rect_t *rect);
 
-    void rtgui_textbox_get_edit_rect(struct rtgui_textbox *box, rtgui_rect_t *rect);
-
-    void rtgui_textbox_ondraw(rtgui_textbox_t *box);
+void rtgui_textbox_ondraw(rtgui_textbox_t *box);
 #ifdef __cplusplus
 }
 #endif
