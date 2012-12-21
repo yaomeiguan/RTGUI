123079bc2f3e67ea58b67a9d21e9a8832821432c
Grissiom <chaos.proton@gmail.com>
2012-12-21 10:33:38 +0800

    win32: don't poll the event, but wait for it
    Idea inspired by prife's implementation in bsp/simulator.

--- win32/main.c
+++ win32/main.c
@@ -55,125 +55,122 @@ int main(int argc, char *argv[])
 
 	/* handle SDL event */
 	while ( !done )
-	{
-		/* Check for events */
-		while ( SDL_PollEvent(&event) )
-		{
-			switch (event.type)
-			{
-			case SDL_MOUSEMOTION:
-				{
-					struct rtgui_event_mouse emouse;
-					emouse.parent.type = RTGUI_EVENT_MOUSE_MOTION;
-					emouse.parent.sender = RT_NULL;
-                    emouse.wid = RT_NULL;
+    {
+        /* Check for events */
+        SDL_WaitEvent(&event);
+        switch (event.type)
+        {
+        case SDL_MOUSEMOTION:
+            {
+                struct rtgui_event_mouse emouse;
+                emouse.parent.type = RTGUI_EVENT_MOUSE_MOTION;
+                emouse.parent.sender = RT_NULL;
+                emouse.wid = RT_NULL;
 
-					emouse.x = ((SDL_MouseMotionEvent*)&event)->x;
-					emouse.y = ((SDL_MouseMotionEvent*)&event)->y;
+                emouse.x = ((SDL_MouseMotionEvent*)&event)->x;
+                emouse.y = ((SDL_MouseMotionEvent*)&event)->y;
 
-					/* init mouse button */
-					emouse.button = button_state;
+                /* init mouse button */
+                emouse.button = button_state;
 
-					/* send event to server */
-					rtgui_server_post_event(&emouse.parent, sizeof(struct rtgui_event_mouse));
-				}
-				break;
+                /* send event to server */
+                rtgui_server_post_event(&emouse.parent, sizeof(struct rtgui_event_mouse));
+            }
+            break;
 
-			case SDL_MOUSEBUTTONDOWN:
-			case SDL_MOUSEBUTTONUP:
-				{
-					struct rtgui_event_mouse emouse;
-					SDL_MouseButtonEvent* mb;
+        case SDL_MOUSEBUTTONDOWN:
+        case SDL_MOUSEBUTTONUP:
+            {
+                struct rtgui_event_mouse emouse;
+                SDL_MouseButtonEvent* mb;
 
-					emouse.parent.type = RTGUI_EVENT_MOUSE_BUTTON;
-					emouse.parent.sender = RT_NULL;
-                    emouse.wid = RT_NULL;
+                emouse.parent.type = RTGUI_EVENT_MOUSE_BUTTON;
+                emouse.parent.sender = RT_NULL;
+                emouse.wid = RT_NULL;
 
-					mb = (SDL_MouseButtonEvent*)&event;
+                mb = (SDL_MouseButtonEvent*)&event;
 
-					emouse.x = mb->x;
-					emouse.y = mb->y;
+                emouse.x = mb->x;
+                emouse.y = mb->y;
 
-					/* init mouse button */
-					emouse.button = 0;
+                /* init mouse button */
+                emouse.button = 0;
 
-					/* set emouse button */
-					if (mb->button & (1 << (SDL_BUTTON_LEFT - 1)) )
-					{
-						emouse.button |= RTGUI_MOUSE_BUTTON_LEFT;
-					}
-					else if (mb->button & (1 << (SDL_BUTTON_RIGHT - 1)))
-					{
-						emouse.button |= RTGUI_MOUSE_BUTTON_RIGHT;
-					}
-					else if (mb->button & (1 << (SDL_BUTTON_MIDDLE - 1)))
-					{
-						emouse.button |= RTGUI_MOUSE_BUTTON_MIDDLE;
-					}
+                /* set emouse button */
+                if (mb->button & (1 << (SDL_BUTTON_LEFT - 1)) )
+                {
+                    emouse.button |= RTGUI_MOUSE_BUTTON_LEFT;
+                }
+                else if (mb->button & (1 << (SDL_BUTTON_RIGHT - 1)))
+                {
+                    emouse.button |= RTGUI_MOUSE_BUTTON_RIGHT;
+                }
+                else if (mb->button & (1 << (SDL_BUTTON_MIDDLE - 1)))
+                {
+                    emouse.button |= RTGUI_MOUSE_BUTTON_MIDDLE;
+                }
 
-					if (mb->type == SDL_MOUSEBUTTONDOWN)
-					{
-						emouse.button |= RTGUI_MOUSE_BUTTON_DOWN;
-						button_state = emouse.button;
-					}
-					else
-					{
-						emouse.button |= RTGUI_MOUSE_BUTTON_UP;
-						button_state = 0;
-					}
+                if (mb->type == SDL_MOUSEBUTTONDOWN)
+                {
+                    emouse.button |= RTGUI_MOUSE_BUTTON_DOWN;
+                    button_state = emouse.button;
+                }
+                else
+                {
+                    emouse.button |= RTGUI_MOUSE_BUTTON_UP;
+                    button_state = 0;
+                }
 
 
-					/* send event to server */
-					rtgui_server_post_event(&emouse.parent, sizeof(struct rtgui_event_mouse));
-				}
-				break;
+                /* send event to server */
+                rtgui_server_post_event(&emouse.parent, sizeof(struct rtgui_event_mouse));
+            }
+            break;
 
-			case SDL_KEYUP:
-				{
-					struct rtgui_event_kbd ekbd;
-					ekbd.parent.type	= RTGUI_EVENT_KBD;
-					ekbd.parent.sender	= RT_NULL;
-					ekbd.type = RTGUI_KEYUP;
-					ekbd.wid = RT_NULL;
-					ekbd.mod = event.key.keysym.mod;
-					ekbd.key = event.key.keysym.sym;
+        case SDL_KEYUP:
+            {
+                struct rtgui_event_kbd ekbd;
+                ekbd.parent.type	= RTGUI_EVENT_KBD;
+                ekbd.parent.sender	= RT_NULL;
+                ekbd.type = RTGUI_KEYUP;
+                ekbd.wid = RT_NULL;
+                ekbd.mod = event.key.keysym.mod;
+                ekbd.key = event.key.keysym.sym;
 
-					/* FIXME: unicode */
-					ekbd.unicode = 0;
+                /* FIXME: unicode */
+                ekbd.unicode = 0;
 
-					/* send event to server */
-					rtgui_server_post_event(&ekbd.parent, sizeof(struct rtgui_event_kbd));
-				}
-				break;
+                /* send event to server */
+                rtgui_server_post_event(&ekbd.parent, sizeof(struct rtgui_event_kbd));
+            }
+            break;
 
-			case SDL_KEYDOWN:
-				{
-					struct rtgui_event_kbd ekbd;
-					ekbd.parent.type	= RTGUI_EVENT_KBD;
-					ekbd.parent.sender	= RT_NULL;
-					ekbd.type = RTGUI_KEYDOWN;
-					ekbd.wid = RT_NULL;
-					ekbd.mod = event.key.keysym.mod;
-					ekbd.key = event.key.keysym.sym;
+        case SDL_KEYDOWN:
+            {
+                struct rtgui_event_kbd ekbd;
+                ekbd.parent.type	= RTGUI_EVENT_KBD;
+                ekbd.parent.sender	= RT_NULL;
+                ekbd.type = RTGUI_KEYDOWN;
+                ekbd.wid = RT_NULL;
+                ekbd.mod = event.key.keysym.mod;
+                ekbd.key = event.key.keysym.sym;
 
-					/* FIXME: unicode */
-					ekbd.unicode = 0;
+                /* FIXME: unicode */
+                ekbd.unicode = 0;
 
-					/* send event to server */
-					rtgui_server_post_event(&ekbd.parent, sizeof(struct rtgui_event_kbd));
-				}
-				break;
+                /* send event to server */
+                rtgui_server_post_event(&ekbd.parent, sizeof(struct rtgui_event_kbd));
+            }
+            break;
 
-			case SDL_QUIT:
-				done = 1;
-				break;
+        case SDL_QUIT:
+            done = 1;
+            break;
 
-			default:
-				break;
-			}
+        default:
+            break;
+        }
 
-			SDL_Delay(20);
-		}
 	}
 
 	SDL_Quit();
